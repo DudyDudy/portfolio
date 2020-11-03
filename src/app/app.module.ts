714cc1f397a12817b6bc4dc16e512fde349084fe
ladubetmush <ndubourg1991@gmail.com>
2020-11-03 17:14:45 +0100

    2nd commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/main-page/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    MainPageComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
